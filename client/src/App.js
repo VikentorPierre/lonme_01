ccd72825ddc8d0e87bca2247839f1b3835bfdc5d
Mosdevv <vikentorpierre@gmail.com>
2019-09-24 22:41:45 -0400

    fix the router issue

--- client/src/App.js
+++ client/src/App.js
@@ -3,28 +3,8 @@ import { BrowserRouter, Route, Switch } from "react-router-dom";
 import "./css/base.css";
 import Navbar from "./components/chuck/header";
 import Home from "./components/feed/home";
-// import Login from "./components/auth/loginpage";
-// import Sign from "./components/auth/signuppage";
 import { connect } from "react-redux";
 
-//profile
-// import {
-//   Profile,
-//   ProfileGoals,
-//   ProfileLikes,
-//   ProfileStories
-// } from "./components/profile/prof";
-// //post
-// import CreatePost from "./components/post/postCreate";
-// import NewPost from "./components/include/newPost";
-
-// // auth
-// import LoginPage from "./components/include/loginpage";
-// import SignupPage from "./components/include/signuppage";
-//import Profile from "./components/profile";
-// import Setting from "./components/setting";
-// import PostDetail from "./components/post/postDetail";
-//import Home from "./components/feed/home";
 import Routes from "./components/routing/Routes";
 import { Provider } from "react-redux";
 import store from "./store";
@@ -40,32 +20,13 @@ class App extends Component {
   render() {
     return (
       <Provider store={store}>
-        <BrowserRouter>
+        <BrowserRouter basename={window.location.pathname || ''} >
           <div className="App">
             <Navbar />
             <Switch>
               <Route exact path='/' component={Home} />
-              {/* <Route exact={true} path="/login" component={Login} />
-              <Route exact={true} path="/signUp" component={Sign} /> */}
-
               <Route component={Routes} />
             </Switch>
-            {/* Feed */}
-            {/* <Route exact={true} path="/" component={Home} /> */}
-            {/* Auth  */}
-            {/* <Route exact={true} path="/login" component={LoginPage} />
-            <Route exact={true} path="/signup" component={SignupPage} /> */}
-            {}
-            {/* Profile */}
-            {/* <Route exact={true} path="/profile" component={Profile} />
-            <Route path="/profile/goals" component={ProfileGoals} />
-            <Route path="/profile/stories/" component={ProfileStories} />
-            <Route path="/profile/like" component={ProfileLikes} /> */}
-            {/* Post */}
-            {/* <Route path="/p/:id/" component={PostDetail} />
-            <Route exact={true} path="/post/create" component={CreatePost} /> */}
-            {/* Setting */}
-            {/* <Route path="/setting/" component={Setting} /> */}
           </div>
         </BrowserRouter>
       </Provider>
