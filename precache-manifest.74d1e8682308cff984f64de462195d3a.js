3fa10316b3cd523841896436f9bf48f7c14c90dc
Mosdevv <vikentorpierre@gmail.com>
2019-09-24 22:37:33 -0400

    Updates

---
+++ precache-manifest.74d1e8682308cff984f64de462195d3a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c59c36291ba1746b8d4f7da5fc0f03a",
+    "url": "/lonme_01/index.html"
+  },
+  {
+    "revision": "2fc95d7d38062e719bca",
+    "url": "/lonme_01/static/css/main.e2783d0f.chunk.css"
+  },
+  {
+    "revision": "e5fa4c5403b34974e9ed",
+    "url": "/lonme_01/static/js/2.7f228412.chunk.js"
+  },
+  {
+    "revision": "2fc95d7d38062e719bca",
+    "url": "/lonme_01/static/js/main.7c3a13fe.chunk.js"
+  },
+  {
+    "revision": "6cd5ca577fcee0dda826",
+    "url": "/lonme_01/static/js/runtime~main.ab15e646.js"
+  },
+  {
+    "revision": "a04d849cf591c2f980548b982f461401",
+    "url": "/lonme_01/static/media/null.a04d849c.jpg"
+  }
+]);
